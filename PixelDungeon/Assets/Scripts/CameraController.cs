473396eae631da6ea2290b0bdc79ad987b63791d
Ahn, Soyeon <esselasy@gmail.com>
2019-05-07 04:26:04 +0900

    #7 휠 스크롤에 따른 카메라 확대/축소

--- PixelDungeon/Assets/Scripts/CameraController.cs
+++ PixelDungeon/Assets/Scripts/CameraController.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     GameObject player;
+    new Camera camera;
+
+    float size = 9;         // Orthographic camera size
+    float zoomSpeed = 10;   // 줌 스피드.
 
     // Start is called before the first frame update
     void Awake()
     {
         player = GameObject.Find("Player");
+        camera = GetComponent<Camera>();
     }
 
     // Update is called once per frame
@@ -19,6 +24,19 @@ public class CameraController : MonoBehaviour
         {
             Vector3 playerPos = player.transform.position;
             transform.position = new Vector3(playerPos.x, playerPos.y, transform.position.z);
-        }        
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+            print("Zoom " + Input.GetAxis("Mouse ScrollWheel"));
+            Zoom();
+        }
+    }
+
+    void Zoom()
+    {
+        size -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        size = Mathf.Clamp(size, 5, 25);
+        camera.orthographicSize = size;
     }
 }
