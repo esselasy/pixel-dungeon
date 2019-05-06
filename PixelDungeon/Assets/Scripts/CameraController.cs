1b9bdc2275153d288f60759c899835a17decea9c
Ahn, Soyeon <esselasy@gmail.com>
2019-05-07 03:47:43 +0900

    #7 카메라가 플레이어를 따라다니게 한다.

---
+++ PixelDungeon/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            Vector3 playerPos = player.transform.position;
+            transform.position = new Vector3(playerPos.x, playerPos.y, transform.position.z);
+        }        
+    }
+}
